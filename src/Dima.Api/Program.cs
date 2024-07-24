380a367551693b83d0f9e244437a11b6e00034d5
Maykon Mendel <maykonmendel.dev@gmail.com>
2024-07-23 21:22:12 -0300

    feat: add EntityFramework

--- src/Dima.Api/Program.cs
+++ src/Dima.Api/Program.cs
@@ -1,5 +1,28 @@
+using Dima.Api.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder
+    .Configuration
+    .GetConnectionString("DefaultConnection") ?? string.Empty;
+
+builder.Services.AddDbContext<AppDbContext>(
+    x =>
+    {
+        x.UseSqlServer(connectionString);
+    }
+);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(x =>
+    x.CustomSchemaIds(n => n.FullName)
+);
+
 var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.MapGet("/", () => "Hello World!");
 
